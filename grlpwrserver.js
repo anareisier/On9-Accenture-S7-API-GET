70afd69efa6ada73655130b43fe866ce99b562c6
Ana Reis <anareis1706@gmail.com>
2020-09-25 22:48:57 -0300

    Entrega API semana 7 - Ana Reis

---
+++ grlpwrserver.js
@@ -0,0 +1,43 @@
+const express =  require('express');
+const app = express();
+const porta = 3000;
+const grlpwr = require('./grlpwr.json')
+
+app.use(express.json());
+
+//[GET]; http://localhost:3000/grlpwr
+app.get('/grlpwr', function(req, res){
+    res.json(grlpwr);
+});
+
+app.get('/grlpwr/:id', function(req, res){
+    const {id} = req.params;
+    const encontrar = grlpwr.find(gp => gp.id = id);
+
+    if(!encontrar) return res.status(204).json();
+
+    res.json(encontrar);
+});
+
+app.listen(porta, function(){
+    console.log("Servidor rodando")
+})
+
+
+/*
+const express =  require('express');
+const app = express();
+const porta = 3000;
+const users = require('./users.json')
+
+app.use(express.json());
+
+//[GET]; http://localhost:3000/users
+app.get('/users', function(req, res){
+    res.json(users);
+})
+
+
+app.listen(porta, function(){
+    console.log("Servidor rodando")
+})*/
